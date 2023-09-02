3fa8961f966fe54d6dfea52cb4ba4822b7119db3
hjaox <hectorjana.o@gmail.com>
2023-09-02 23:09:23 +0100

    do about-me and description stuff

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const mainEle = document.getElementById("main");
 const flashlight = document.getElementById("flashlight");
+
+const mainEle = document.getElementById("main");
+
 const aboutEle = document.getElementById("about");
 const projectsEle = document.getElementById("projects");
 const experienceEle = document.getElementById("experience");
+
 const aboutBulletEle = document.getElementById("list-bullet-about");
 const projectsBulletEle = document.getElementById("list-bullet-projects");
 const experienceBulletEle = document.getElementById("list-bullet-experience");
 
+const aboutList = document.getElementById('li-about')
+const projectsList = document.getElementById('li-projects')
+const experienceList = document.getElementById('li-experience')
+
+const htmlTest = document.getElementById('skills-list-html')
+
 eleTopListen();
+handleNavOpacity();
 
 for (const bullet of document.querySelectorAll(".list-nav-a")) {
-  bullet.addEventListener("mouseover", (event) => handleMouseOverEvent(bullet));
-}
-
-for (const bullet of document.querySelectorAll(".list-nav-a")) {
+  bullet.addEventListener("mouseover", (event) => handleBulletMouseOverEvent(bullet));
   bullet.addEventListener("mouseout", () => eleTopListen());
 }
 
-function handleMouseOverEvent(bullet) {
+function handleBulletMouseOverEvent(bullet) {
   if (bullet.classList.contains("nav-about")) {
     aboutBulletEle.style.setProperty("width", "4rem");
   }
@@ -36,6 +43,7 @@ mainEle.addEventListener("scroll", (event) => {
 mainEle.addEventListener("mousemove", (event) => {
   let xLoc = event.clientX;
   let yLoc = event.clientY;
+  handleNavOpacity()
   flashlight.style.setProperty("--x", `${xLoc}px`);
   flashlight.style.setProperty("--y", `${yLoc}px`);
 });
@@ -45,15 +53,39 @@ function eleTopListen() {
   let projectsTop = projectsEle.getBoundingClientRect().top;
   let experienceTop = experienceEle.getBoundingClientRect().top;
 
-  aboutTop > -175 && aboutTop <= 38
+  !(projectsTop <= 35) && aboutTop <= 100
     ? aboutBulletEle.style.setProperty("width", "4rem")
     : aboutBulletEle.style.setProperty("width", "2rem");
 
-  projectsTop > -983 && projectsTop <= 35
+  !(experienceTop <= 35) && projectsTop <= 35
     ? projectsBulletEle.style.setProperty("width", "4rem")
     : projectsBulletEle.style.setProperty("width", "2rem");
 
-  experienceTop >= -500 && experienceTop <= 35
+  experienceTop <= 35
     ? experienceBulletEle.style.setProperty("width", "4rem")
     : experienceBulletEle.style.setProperty("width", "2rem");
+}
+
+function handleNavOpacity() {
+  let aboutBulletEleWidth = aboutBulletEle.attributes.style.value.split(' ')[1];
+  let projectsBulletEleWidth = projectsBulletEle.attributes.style.value.split(' ')[1];
+  let experienceBulletEleWidth = experienceBulletEle.attributes.style.value.split(' ')[1];
+
+  if(aboutBulletEleWidth === '4rem;') {
+    aboutList.style.setProperty("opacity", "1")
+  } else {
+    aboutList.style.setProperty("opacity", "0.3")
+  }
+
+  if(projectsBulletEleWidth === '4rem;') {
+    projectsList.style.setProperty("opacity", "1")
+  } else {
+    projectsList.style.setProperty("opacity", "0.3")
+  }
+
+  if(experienceBulletEleWidth === '4rem;') {
+    experienceList.style.setProperty("opacity", "1")
+  } else {
+    experienceList.style.setProperty("opacity", "0.3")
+  }
 }
