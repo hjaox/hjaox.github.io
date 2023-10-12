de81f49c0abba44e100406a034658881f66dc0c7
hjaox <hectorjana.o@gmail.com>
2023-10-12 15:33:17 +0100

    wE

---
+++ src/data/workExperience.js
@@ -0,0 +1,43 @@
+export const workExperience = {
+    'Golden Sky Trading & Engineering Services WLL': {
+        role: 'Site Engineer',
+        location: 'Qatar',
+        date: 'July 2015 - March 2016',
+        info: ['Design and fabricate Motor Control Center Panels (MCP',
+    ' Supervision of Manpower',
+    'Assisting BMS Engineer in Preventive Maintenance works and other activities']
+    },
+    'First Oceanic Property Management Inc.': {
+        role: 'Building Engineer',
+        location: '',
+        date: 'March 2017 - May 2018',
+        info: ['Ensures the smooth flowing and efficiency of operation of the building equipment.',
+    'Supervision of Manpower (Security, Housekeeping and Maintenance Crew)',
+    ]
+    },
+    'Bake n\'Cake': {
+        role: '',
+        location: '',
+        date: '',
+        info: ''
+    },
+    'Admin Staff': {
+        role: '',
+        location: '',
+        date: '',
+        info: ''
+    },
+    'Plant Operator': {
+        role: '',
+        location: '',
+        date: '',
+        info: ''
+    },
+    'Northcoders': {
+        role: '',
+        location: '',
+        date: '',
+        info: ''
+    },
+    
+}
