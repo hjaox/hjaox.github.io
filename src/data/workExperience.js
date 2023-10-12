5b130efbe8f53ea1532858565f305ee83bb056e9
hjaox <hectorjana.o@gmail.com>
2023-10-12 16:03:07 +0100

    wE finished

--- src/data/workExperience.js
+++ src/data/workExperience.js
@@ -9,35 +9,42 @@ export const workExperience = {
     },
     'First Oceanic Property Management Inc.': {
         role: 'Building Engineer',
-        location: '',
+        location: 'Philippines',
         date: 'March 2017 - May 2018',
-        info: ['Ensures the smooth flowing and efficiency of operation of the building equipment.',
+        info: ['Ensures the smooth flowing and efficiency of operation of the building equipment',
     'Supervision of Manpower (Security, Housekeeping and Maintenance Crew)',
-    ]
+    'Provides relevant data regarding the daily and monthly status of equipment and act accordingly if intervention from other vendors is necessary',
+    ' Perform basic in-house Preventive Maintenance daily and monthly of building equipment']
     },
     'Bake n\'Cake': {
-        role: '',
-        location: '',
-        date: '',
-        info: ''
+        role: 'Technician',
+        location: 'UAE',
+        date: 'November 2019 - March 2020',
+        info: ['Ensures the smooth flowing and efficiency of operation of the building equipment',
+    'Repair and conduct maintenance to equipment.']
     },
-    'Admin Staff': {
-        role: '',
-        location: '',
-        date: '',
-        info: ''
+    'Clippers Logistics (PE3) under Staffing Match Agency': {
+        role: 'Admin Staff',
+        location: 'Peterborough, UK',
+        date: 'August 2021 - February 2022',
+        info: ['Allocating orders for E-commerce and Retail customers',
+    'Cooperating with other departments with investigating problematic orders']
     },
-    'Plant Operator': {
-        role: '',
-        location: '',
-        date: '',
-        info: ''
+    'SUEZ Water Technologies and Solutions UK L P': {
+        role: 'Plant Operator',
+        location: 'Orton Southgate',
+        date: 'Feb 2022 - Sept 2022',
+        info: ['Ensuring the quality (QC) of different types of water treatment equipment (Deionization & Reverse Osmosis). This can include mechanical and electrical jobs',
+    'Troubleshooting of electrical and control systems in our fleet equipment',
+    'Producing documents on the work done i.e., production and quality control paperwork and other computer related tasks',
+    ' Ensuring all duties are performed in a safe manner within the site rules & current health and safety legislation',
+    'Execute all other tasks which are assigned by the manager/ shift leader']
     },
     'Northcoders': {
         role: '',
         location: '',
         date: '',
-        info: ''
+        info: ['During the 13-week Bootcamp where I learned the fundamentals of Full-Stack Development, I acquired the fundamentals of JavaScript which helped me in creating a back-end service to provide data for a front-end architecture that I have built. \n\n All throughout the process, I aimed to build clean testable codes by following Test Driven Development process with agile methodologies while both working solo or working with colleagues through pair-programming or in group projects']
     },
     
 }
