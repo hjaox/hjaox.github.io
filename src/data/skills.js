0ea4c44d72bb486d879b2bcfe391e1067e962659
hjaox <hectorjana.o@gmail.com>
2023-10-12 14:12:03 +0100

    starting react version

---
+++ src/data/skills.js
@@ -0,0 +1,34 @@
+export const languages = [
+    'JavaScript'
+];
+
+export const frontend = [
+    'HTML',
+    'CSS',
+    'React',
+    'React-Native',
+    'Axios'
+];
+
+export const backend = [
+    'PostgreSQL',
+    'node-Postgres',
+    'Express',
+    'Node.js'
+];
+
+export const testing = [
+    'Test-Driven Development (TDD)',
+    'Jest',
+    'Supertest'
+];
+
+export const hosting = [
+    'Render',
+    'Netlify',
+    'ElephantSQL'
+];
+
+export const others = [
+    'Object-Oriented Programming (OOP)'
+];
