37900aadcf0792a3a0a6cf8e110c0252e9849e30
hjaox <hectorjana.o@gmail.com>
2023-10-20 22:54:37 +0100

    temp save

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { useEffect } from "react";
 import "./App.css";
 import LeftContainer from "./components/LeftContainer/LeftContainer";
 import RightContainer from "./components/RightContainer/RightContainer";
 
 function App() {
+  useEffect(() => {
+    const flashlight = document.getElementById("flashlight");
+    const mainEle = document.getElementById("main");
+    mainEle.addEventListener("mousemove", event => {
+      let xLoc = event.clientX;
+      let yLoc  = event.clientY;
+      console.log(xLoc, yLoc);
+      flashlight.style.setProperty("--x", `${xLoc}px`);
+      flashlight.style.setProperty("--y", `${yLoc}px`);
+      
+    })
+  },[])
+
   return (
-    <main className="lg:flex lg:justify-between lg:px-10 lg:gap-4  mx-auto max-w-screen-xl relative font-poppins">
+    <> 
+    <div id="flashlight" className="absolute inset-0" style={{background:`radial-gradient(800px circle at var(--x) var(--y), rgba(29, 78, 216, 0.15), transparent 80%)`}}></div>
+    <main id="main" className="lg:flex lg:justify-between lg:px-10 lg:gap-4  mx-auto max-w-screen-xl relative font-poppins">
       <LeftContainer />
       <RightContainer />
     </main>
+    </>
   );
 }
 
